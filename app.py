a624a05a9d91c828ce7b4f3655e8dcede011b23f
André Díaz <andre290800@gmail.com>
2023-03-06 01:03:33 -0600

    feat: ✨ animate player

--- app.py
+++ app.py
@@ -93,23 +93,30 @@ class Player(pygame.sprite.Sprite):
         self.mask = None
         self.direction: Union[Direction, None] = None
         self.animation_count = 0
+        self.sprites = load_sprites("", size, True)
+        self.sprite = "idle_right"
 
     def move(self, keys: pygame.key.ScancodeWrapper) -> None:
         """Mueve al jugador"""
         if keys[pygame.K_LEFT]:
             self.rect.x -= self.velocity
             self.set_direction(Direction.left)
+            self.sprite = "run_left"
         elif keys[pygame.K_RIGHT]:
             self.rect.x += self.velocity
             self.set_direction(Direction.right)
+            self.sprite = "run_right"
         elif keys[pygame.K_UP]:
             self.rect.y -= self.velocity
             self.set_direction(Direction.up)
+            self.sprite = "jump" + ("_right" if "right" in self.sprite else "_left")
         elif keys[pygame.K_DOWN]:
             self.rect.y += self.velocity
             self.set_direction(Direction.down)
+            self.sprite = "fall" + ("_right" if "right" in self.sprite else "_left")
         else:
             self.set_direction(None)
+            self.sprite = "idle" + ("_right" if "right" in self.sprite else "_left")
 
     def set_direction(self, direction: Union[Direction, None]) -> None:
         """Cambia la dirección del jugador"""
@@ -119,7 +126,8 @@ class Player(pygame.sprite.Sprite):
 
     def draw(self) -> None:
         """Dibuja al jugador"""
-        pygame.draw.rect(screen, self.color, self.rect)
+        screen.blit(self.sprites[self.sprite][self.animation_count], self.rect)
+        self.animation_count = (self.animation_count + 1) % len(self.sprites[self.sprite])
 
 
 def draw(player: Player) -> None:
